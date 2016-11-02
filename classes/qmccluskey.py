66ec30f3b0b4bf7c61b87dafa9c076c22e0d459a
gisliany <gisliany.alves@gmail.com>
2016-11-01 23:36:37 -0300

    QuineMcCluskey finalized

--- classes/qmccluskey.py
+++ classes/qmccluskey.py
@@ -2,9 +2,11 @@ class QuineMcCluskey:
 
     def __init__(self, expression):
         self.__expression = expression
+        self.__prime_implicants = []
 
     def __group_by_ones(self):
         groups = {}
+        sorted_groups = {}
         terms_number = len(self.__expression)
         term_len = len(self.__expression[0])
 
@@ -14,48 +16,59 @@ class QuineMcCluskey:
                 if self.__expression[i][j] == '1':
                     ones_number += 1
 
-            if not groups.has_key(str(ones_number)):
-                groups[str(ones_number)] = []
-            groups[str(ones_number)].append(self.__expression[i])
+            if not groups.has_key(ones_number):
+                groups[ones_number] = []
+            groups[ones_number].append(self.__expression[i])
 
-        return groups
+        for key in sorted(groups):
+            sorted_groups[key] = groups[key]
 
-    def get_expression(self): # mudar para property
-        return self.__expression
+        return sorted_groups
 
+    def __init_marks(self, groups):
+        marks = {}
+        for key, value in groups.iteritems():
+            marks[key] = len(value)*[False]
+
+        return marks
+
+    def get_prime_implicants(self): # mudar para property
+        return self.__prime_implicants
 
     def resolve(self):
         groups = self.__group_by_ones()
+        marks = self.__init_marks(groups)
         term_len = len(self.__expression[0])
-        exp = []
+        miniterms = []
         execute_again = False
 
-        print "GROUPS \n"
-        print groups
-
-        for i in range(term_len):
-            if groups.has_key(str(i)) and groups.has_key(str(i + 1)):
-                for k in range(len(groups[str(i)])):
-                    for m in range(len(groups[str(i + 1)])):
-
+        for key_ones, terms_list in groups.iteritems():
+            if groups.has_key(key_ones + 1):
+                for k in range(len(terms_list)):
+                    for m in range(len(groups[key_ones + 1])):
                         diff_bit_index = 0
                         diff_bit_number = 0
                         for n in range(term_len):
-
-                            if groups[str(i)][k][n] != groups[str(i + 1)][m][n]:
+                            if terms_list[k][n] != groups[key_ones + 1][m][n]:
                                 diff_bit_index = n
                                 diff_bit_number += 1
 
                         if diff_bit_number == 1:
                             execute_again = True
-                            term = groups[str(i)][k][:diff_bit_index] + '-' + groups[str(i)][k][diff_bit_index + 1:]
-                            #print groups[str(i)][k] + " and " + groups[str(i + 1)][m] + "\n"
-                            exp.append(term)
+                            term = terms_list[k][:diff_bit_index] + '-' + terms_list[k][diff_bit_index + 1:]
+                            marks[key_ones][k] = True
+                            marks[key_ones + 1][m] = True
+                            miniterms.append(term)
+
+        for key, mark_list in marks.iteritems():
+            for index, mark in enumerate(mark_list):
+                if not mark:
+                    self.__prime_implicants.append(groups[key][index])
+
 
         if execute_again:
-            if len(exp) > 0:
-                self.__expression = list(set(exp))
+            if len(miniterms) > 0:
+                self.__expression = list(set(miniterms))
             self.resolve()
         else:
-            #print self.__expression
-            return self.__expression
+            self.__prime_implicants = list(set(self.__expression + self.__prime_implicants))
