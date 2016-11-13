02124dd3aa929a320e7acbe733b88f82d31a7190
gisliany <gisliany.alves@gmail.com>
2016-11-13 16:04:55 -0300

    DFS class implemented

--- classes/dfs.py
+++ classes/dfs.py
@@ -1,6 +1,18 @@
 class DFS:
+    '''
+    DFS class
+        This class implements Depth-First Search to search all paths between two specific nodes.
+
+    Attributes:
+        __source: a string representing the source id
+        __target: a string representing the target id
+        __topology: a dictionary whoses indexes are node ids and the values are lists of adjacents nodes (id)
+        __visited: a list with the ids of the visited nodes
+        __paths: a list with all paths between the source and the target
+    '''
 
     def __init__(self, source, target, topology):
+        ''' This constructor initializes the class attributes. '''
         self.__source = source
         self.__target = target
         self.__topology = topology
@@ -9,10 +21,12 @@ class DFS:
         self.__paths = []
 
     def getPaths(self):
+        ''' getPaths public method returns a list with all paths found. '''
         return self.__paths
 
     def execute(self):
-        nodes = self.__topology[self.__visited[-1]] #getting adjacents nodes of the last node visited
+        ''' execute public method executes the DFS and fills the __paths attribute. '''
+        nodes = self.__topology[self.__visited[-1]] # getting adjacents nodes of the last node visited
 
         for node in nodes:
             if node in self.__visited:
